79b35bb92982441d08959d1507ac89c49a11ea43
patrickbies <patjbies@gmail.com>
2025-02-12 17:51:17 -0500

    reworking - done exams can now start website

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,14 +4,14 @@ import AppRoutes from "./routes/AppRoutes.tsx"
 function App() {
   const savedTheme = localStorage.getItem('theme');
   const initialTheme = savedTheme || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-  const [theme, setTheme] = useState(initialTheme);
+  const [theme, setTheme] = useState('light');
 
   // Set the theme class on the root element
   useEffect(() => {
     if (theme === 'dark') {
       document.documentElement.classList.add('dark');
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove('light');
     }
   }, [theme]);
 
--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -2,14 +2,8 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="bg-[#CBD0B9] w-full h-screen flex flex-col">
-      <div className="text-[#514B23] poppins-black leading-none text-[13.5vw] flex justify-between">
-        <span>PATRICK</span> <span>BIES</span>
-      </div>
-      <div className="text-5xl md:text-6xl flex flex-col w-fit">
-        <span className="text-[#514B23] poppins-bold text-left">SOFTWARE</span>
-        <span className="text-[#656839] poppins-bold text-left">DEVELOPER</span>
-      </div>
+    <div className="bg-background w-full h-screen px-[10vw] py-[4vh]">
+      <h1 className="text-3xl poppins-semibold text-foreground">Patrick Bies</h1>
     </div>
   );
 };
