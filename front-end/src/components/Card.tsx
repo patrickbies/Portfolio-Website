ebc3e2f48908767c210b39f826ced5471392d5e3
patrickbies <patjbies@gmail.com>
2025-03-05 18:37:24 -0500

    a

--- front-end/src/components/Card.tsx
+++ front-end/src/components/Card.tsx
@@ -1,10 +1,15 @@
-const Card = ({title, desc} : {title : string, desc : string}) => {
+const Card = ({ title, desc }: { title: string; desc: string }) => {
   return (
-    <div className="w-full h-full rounded-2xl border-border hover:border-foreground/80 transition-colors border cursor-pointer px-10">
-      <h1 className="poppins-medium text-xl py-8">{title}</h1>
-      <p className="poppins-regular text-lg text-foreground/80">{desc}</p>
+    <div className="w-full h-full rounded-2xl border-border hover:border-foreground/80 transition-colors border cursor-pointer px-10 flex flex-col group">
+      <h1 className="poppins-medium text-xl py-8 text-foreground/90 transition-colors group-hover:text-foreground">
+        {title}
+      </h1>
+      <p className="poppins-regular text-lg text-foreground/60 transition-colors group-hover:text-foreground/80">
+        {desc}
+      </p>
+      <div className="flex-grow rounded-2xl border border-border transition-colors group-hover:border-foreground/80 my-8"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
